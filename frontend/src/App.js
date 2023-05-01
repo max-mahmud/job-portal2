bf45eda25587efacabe2cc2cead2a60bfa84ffb2
max-mahmud <newmahmud24@gmail.com>
2023-05-01 17:33:21 +0600

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,17 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './theme';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Footer from './component/Footer'
 import Navbar from './component/Navbar'
+import LogIn from './pages/LogIn';
 
 const App = () => {
 
     return (
         <>
+        <ToastContainer />
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <BrowserRouter>
@@ -20,6 +23,7 @@ const App = () => {
                         <Route path='/' element={<Home />} />
                         <Route path='/search/location/:location' element={<Home />} />
                         <Route path='/search/:keyword' element={<Home />} />
+                        <Route path='/login' element={<LogIn />} />
                         <Route path='*' element={<NotFound />} />
                     </Routes>
                     <Footer />
